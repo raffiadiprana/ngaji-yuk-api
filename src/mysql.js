f23e2e363ecbcf07cfbd58530f8e7dc19e4f0c7b
raffiadiprana <raffiadiprana469@gmail.com>
2026-07-04 14:33:05 +0700

    add SSL support for production database

--- src/mysql.js
+++ src/mysql.js
@@ -1,8 +1,12 @@
-// For more information about this file see https://dove.feathersjs.com/guides/cli/databases.html
 import knex from 'knex'
 
 export const mysql = app => {
   const config = app.get('mysql')
+
+  if (process.env.MYSQL_SSL === 'true') {
+    config.connection.ssl = { rejectUnauthorized: false }
+  }
+
   const db = knex(config)
 
   app.set('mysqlClient', db)
