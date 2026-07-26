2f09d1d928c4c967fcf99d7bea3170f4c7914071
raffiadiprana <raffiadiprana469@gmail.com>
2026-07-26 09:40:30 +0700

    fix: support ssl on string DATABASE_URL connection

--- src/postgresql.js
+++ src/postgresql.js
@@ -4,7 +4,14 @@ export const postgresql = app => {
   const config = app.get('postgresql')
 
   if (process.env.PG_SSL === 'true' || process.env.DATABASE_SSL === 'true') {
-    config.connection.ssl = { rejectUnauthorized: false }
+    if (typeof config.connection === 'string') {
+      config.connection = {
+        connectionString: config.connection,
+        ssl: { rejectUnauthorized: false }
+      }
+    } else {
+      config.connection.ssl = { rejectUnauthorized: false }
+    }
   }
 
   const db = knex(config)
